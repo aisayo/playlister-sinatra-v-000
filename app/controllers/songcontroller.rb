05b19d94edeec639c82eac87338542099d651c26
aisayo <aisayo@icloud.com>
2017-10-15 22:06:03 +0000

    automatically backed up by learn

--- app/controllers/songcontroller.rb
+++ app/controllers/songcontroller.rb
@@ -39,7 +39,7 @@ class SongController < ApplicationController
     if !params[:artist_name].empty?
     @song.genres = params[:genres].map {|g| Genre.find_by(name: g)}
     @song.save
-  end
+    end
     flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
