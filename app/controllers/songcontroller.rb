b24517f8abd6c0c4d3a09176cfa2703ca3e42aa1
aisayo <aisayo@icloud.com>
2017-10-15 22:04:43 +0000

    automatically backed up by learn

--- app/controllers/songcontroller.rb
+++ app/controllers/songcontroller.rb
@@ -35,9 +35,9 @@ class SongController < ApplicationController
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-    @song.artist = Artist.find_or_create_by(name: params[:artist_name]) 
+    @song.artist = Artist.find_or_create_by(name: params[:artist_name])
     if !params[:artist_name].empty?
-    @song.genres = params[:genres].map {|g| Genre.find_by(name: g)}
+      @song.genres = params[:genres].map {|g| Genre.find_by(name: g)}
     @song.save
     flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
